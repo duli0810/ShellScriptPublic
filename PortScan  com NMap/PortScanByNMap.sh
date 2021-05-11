5d99a890f80cc68aa4cd712918223aa07d592c4b
Eduardo Rodrigues da Silva <83621137+duli0810@users.noreply.github.com>
2021-05-11 12:11:20 -0300

    Update PortScanByNMap.sh

--- PortScan  com NMap/PortScanByNMap.sh
+++ PortScan  com NMap/PortScanByNMap.sh
@@ -1,13 +1,27 @@
 #!/bin/bash
 
-# Nmap 
+echo ""
+echo                                              "################ Script - Script PortScan NMap ################"
+echo ""
+echo                                                  "################ Autor: EDUARDO RODRIGUES ################"
+echo ""
+echo                                                   "################ Criado em: 14/04/2021 ################"
+echo ""
+echo ""
+echo ""
+"
+
+Acesse o Blog e veja o passo a passo: https://acesso8.blogspot.com/
+  
+"
+
 clear 
 
-for ip in $(cat /home/suporte/Script/ip.txt)
+for ip in $(cat /home/acesso8/Script/ip.txt)
 
 do
 
-nmap -sS -vv $ip | grep "Discovered open port" > /home/suporte/PortScan/$ip.txt 
+nmap -sS -vv $ip | grep "Discovered open port" > /home/acesso8/PortScan/$ip.txt 
 
 done
 
