5311cccf9338afdb56625d49be21d09914a600f1
Eduardo Rodrigues da Silva <edu0810@gmail.com>
2021-09-05 18:37:01 -0300

    Update PortScanByNMap.sh

--- PortScan  com NMap/PortScanByNMap.sh
+++ PortScan  com NMap/PortScanByNMap.sh
@@ -11,17 +11,17 @@ echo ""
 echo ""
 "
 
-Acesse o Blog e veja o passo a passo: https://acesso8.blogspot.com/
+Acesse o Blog e veja o passo a passo: https://acesso8.blogspot.com/2021/05/portscan-automatizado-com-nmap.html
   
 "
 
 clear 
 
-for ip in $(cat /home/acesso8/Script/ip.txt)
+for ip in $(cat <Path do home do Usuario>/Script/ip.txt) # Informe a Path do home do Usuario Ex.: /root/Script ( para root ) ou /home/acesso8 ( para um não SuperUsuario )
 
 do
 
-nmap -sS -vv $ip | grep "Discovered open port" > /home/acesso8/PortScan/$ip.txt 
+nmap -sS -vv $ip | grep "Discovered open port" > <Path do home do Usuario>/Script/PortScan/$ip.txt # Informe a Path do home do Usuario Ex.: /root/Script ( para root ) ou /home/acesso8 ( para um não SuperUsuario )
 
 done
 
