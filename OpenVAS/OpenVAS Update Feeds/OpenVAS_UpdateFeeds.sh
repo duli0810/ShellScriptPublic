61d58d9d9e53fbdb86f6b6ee15f60bdd08207f86
Eduardo Rodrigues da Silva <edu0810@gmail.com>
2021-09-08 16:51:12 -0300

    Update OpenVAS_UpdateFeeds.sh

--- OpenVAS/OpenVAS Update Feeds/OpenVAS_UpdateFeeds.sh
+++ OpenVAS/OpenVAS Update Feeds/OpenVAS_UpdateFeeds.sh
@@ -1,7 +1,24 @@
 #!/bin/bash
-
-# No Debian, o comando deve ser executado como usuário _gvm.: sudo runuser -u _gvm - greenbone-nvt-sync
-
+echo ""
+echo                                               "################ Script - OpenVAS Update Feeds ################"
+echo ""
+echo                                                  "################ Autor: EDUARDO RODRIGUES ################"
+echo ""
+echo                                                   "################ Criado em: 08/09/2021 ################"
+echo ""
+echo ""
+echo "
+############################################################################
+#                                                                          #
+#  Acesse o Blog e veja o passo a passo: https://acesso8.blogspot.com/     #
+#                                                                          #
+############################################################################
+"
+echo ""
+echo ""
+echo "No Debian, o comando deve ser executado como usuário _gvm.: sudo runuser -u _gvm - greenbone-nvt-sync"
+echo "Faça alteração no script do comando sudo greenbone-nvt-sync para sudo runuser -u _gvm - greenbone-nvt-sync"
+echo ""
 sudo greenbone-nvt-sync
 sudo greenbone-scapdata-sync
 sudo greenbone-certdata-sync
